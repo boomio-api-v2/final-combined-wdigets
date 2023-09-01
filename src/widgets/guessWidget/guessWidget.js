31e9bf4713d3e73d142bd717ee491e1e15c4331a
Remigijus66 <remigijus.bartaska@gmail.com>
2023-09-01 15:12:42 +0300

    desktop

--- src/widgets/guessWidget/guessWidget.js
+++ src/widgets/guessWidget/guessWidget.js
@@ -53,81 +53,81 @@ class GuessWidget {
     // <img src=${queIcon} alt="icon"></img>
     myCanvas.innerHTML = `
     <div class="wrapper">
-      <ul class="guess-cards">
-      <li id="cube1" class='cube'>
+      <div class="guess-cards">
+      <div id="cube1" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube2" class='cube'>
+    </div>
+      <div id="cube2" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube3" class='cube'>
+    </div>
+      <div id="cube3" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube4" class='cube'>
+    </div>
+      <div id="cube4" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube5" class='cube'>
+    </div>
+      <div id="cube5" class='cube'>
       <div class="front front-view centertile"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view centertile "></div>
       <div class="left front-view centertile"></div>
       <div class="top front-view centertile"></div>
       <div class="bottom front-view centertile"></div>
-    </li>
-      <li id="cube6" class='cube'>
+    </div>
+      <div id="cube6" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube7" class='cube'>
+    </div>
+      <div id="cube7" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube8" class='cube'>
+    </div>
+      <div id="cube8" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
-      <li id="cube9" class='cube'>
+    </div>
+      <div id="cube9" class='cube invisible'>
       <div class="front front-view question"></div>
       <div class="back front-view"> <img class="cube-image"src=${img1}></div>
       <div class="right front-view"></div>
       <div class="left front-view"></div>
       <div class="top front-view"></div>
       <div class="bottom front-view"></div>
-    </li>
+    </div>
          
-      </ul>
+      </div>
     </div>
     `;
     widgetHtmlService.container.appendChild(myCanvas);
@@ -171,13 +171,7 @@ class GuessWidget {
     console.log('cubes', cubes)
     let arr = [1, 2, 3, 4, 1, 2, 3, 4];
     arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
-    // for (let i = 1; i < 10; i++) {
-    //   const element = document.getElementById(`cube${i}`);
-    //   let imgTag = element.querySelector('img')
-    //   console.log('imgTag ===', imgTag);
-    //   imgTag.src = `https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/quessWidget-new-design/src/widgets/guessWidget/img-${i < 5 ? arr[i-1] : i > 5 ? arr[i - 2] : ''}.png?raw=true`;
-    // }
-
+ 
     cubes.forEach((cube, i) => {
       let imgTag = cube.querySelector('img');
       // card.classList.remove('flip')
@@ -187,35 +181,48 @@ if (i === 4) {imgTag.src = 'https://github.com/boomio-api-v2/final-combined-wdig
     });
 
     // setTimeout(() => {
-    //   for (let i = 1; i < 4; i++) {
-    //     cards[i - 1].classList.add(`flytop${i}`)
-    //     cards[i + 2].classList.add(`flymid${i}`)
-    //     cards[i + 5].classList.add(`flybottom${i}`)
-    //   }
-    //   cards.forEach((card) => {
-    //     card.classList.remove('invisible')
+   
+    //   cubes.forEach((cube) => {
+    //     cube.classList.remove('invisible')
     //   })
 
-    // }, 400);
+    // }, 1500);
+
+    // setTimeout(() => {
+
+    //   cubes.forEach((cube, i) => {
+    //     if (i != 4) {
+    //       cube.classList.remove('turn-front')
+    //       cube.classList.add('turn-back')
+    //       const walls = Array.from(document.querySelectorAll('div'))
+    //       walls.forEach((wall)=> {
+    //         wall.classList.remove('front-view')
+    //         wall.classList.add('back-view')
+    //       })
+    //     }
+    //       })
+
+    // }, 600);
+
+    // setTimeout(() => {
+
+    //   cubes.forEach((cube, i) => {
+    //     if (i != 4) {
+    //       cube.classList.remove('turn-front')
+    //       cube.classList.remove('turn-back')
+    //       const walls = Array.from(document.querySelectorAll('div'))
+    //       walls.forEach((wall)=> {
+    //         wall.classList.remove('front-view')
+    //         wall.classList.add('back-view')
+    //       })
+    //     }
+    //       })
+
+    // }, 2000);
 
     setTimeout(() => {
-
       cubes.forEach((cube, i) => {
-        if (i != 4) {
-          cube.classList.remove('turn-front')
-          cube.classList.add('turn-back')
-          const walls = Array.from(document.querySelectorAll('div'))
-          walls.forEach((wall)=> {
-            wall.classList.remove('front-view')
-            wall.classList.add('back-view')
-          })
-        }
-          })
-
-    }, 500);
-
-    setTimeout(() => {
-      cubes.forEach((cube, i) => {
+        cube.classList.remove('invisible')
         if (i != 4) {
           cube.classList.add('turn-front')
           cube.classList.remove('turn-back')
@@ -226,18 +233,9 @@ if (i === 4) {imgTag.src = 'https://github.com/boomio-api-v2/final-combined-wdig
           })
         }
           })
-    },2000);
+    },1500);
 
-    // setTimeout(() => {
-    //   cards.forEach((card) => {
-    //     card.classList.remove('flip');
-    //   })
-    //   for (let i = 1; i < 4; i++) {
-    //     cards[i - 1].classList.remove(`flytop${i}`)
-    //     cards[i + 2].classList.remove(`flymid${i}`)
-    //     cards[i + 5].classList.remove(`flybottom${i}`)
-    //   }
-    // }, 3000);
+ 
 
 
   }
@@ -289,10 +287,21 @@ if (i === 4) {imgTag.src = 'https://github.com/boomio-api-v2/final-combined-wdig
           wall.classList.remove('back-view')
         })
       }
+      const letFlow = (el) => {
+        el.classList.remove('turn-front')
+        el.classList.remove('turn-back')
+        const walls = Array.from(el.querySelectorAll('div'))
+        walls.forEach((wall)=> {
+          wall.classList.remove('front-view')
+          wall.classList.add('back-view')
+        })
+      }
       if (img1 === img2) {
         matched++;
-        cardOne.classList.add('jump');
-        cardTwo.classList.add('jump');
+        letFlow(cardOne);
+        letFlow(cardTwo);
+        // cardOne.classList.add('jump');
+        // cardTwo.classList.add('jump');
         if (matched == 4) {
           setTimeout(() => {
             const guessContainer = document.getElementById('guess-container');
