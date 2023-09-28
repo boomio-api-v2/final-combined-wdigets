83a1341026864375bf9acc788ad295fcb3b543b3
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2023-09-28 18:02:06 +0300

    testing

--- src/services/boomio.js
+++ src/services/boomio.js
@@ -49,6 +49,7 @@ class BoomioService extends UserService {
   };
 
   setInitialConfiguration() {
+    console.log('dont mind me just testing cloudflare');
     this.config = localStorageService.getDefaultConfig();
     const isTimeout = new Date(this.config.boomioStopTill).getTime() > new Date().getTime();
     if (!isTimeout) {
